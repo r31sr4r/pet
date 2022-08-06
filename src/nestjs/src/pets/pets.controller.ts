2c4e9b0798abe1d23006cce1adca88efc1693396
r31sr4r <rafael.reis.a@gmail.com>
2022-08-06 12:33:10 +0000

    feat: pets controller

--- src/nestjs/src/pets/pets.controller.ts
+++ src/nestjs/src/pets/pets.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PetsService } from './pets.service';
+import {
+  CreatePetUseCase,
+  DeletePetUseCase,
+  GetPetUseCase,
+  ListPetsUseCase,
+  UpdatePetUseCase,
+} from 'pet-core/pet/application';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Inject,
+  Put,
+  HttpCode,
+  Query,
+} from '@nestjs/common';
 import { CreatePetDto } from './dto/create-pet.dto';
+import { SearchPetDto } from './dto/search-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
 
 @Controller('pets')
 export class PetsController {
-  constructor(private readonly petsService: PetsService) {}
+  @Inject(CreatePetUseCase.UseCase)
+  private createUseCase: CreatePetUseCase.UseCase;
+
+  @Inject(UpdatePetUseCase.UseCase)
+  private updateUseCase: UpdatePetUseCase.UseCase;
+
+  @Inject(DeletePetUseCase.UseCase)
+  private deleteUseCase: DeletePetUseCase.UseCase;
+
+  @Inject(GetPetUseCase.UseCase)
+  private getUseCase: GetPetUseCase.UseCase;
+
+  @Inject(ListPetsUseCase.UseCase)
+  private listUseCase: ListPetsUseCase.UseCase;
 
   @Post()
   create(@Body() createPetDto: CreatePetDto) {
-    return this.petsService.create(createPetDto);
+      return this.createUseCase.execute(createPetDto);
   }
 
-  @Get()
-  findAll() {
-    return this.petsService.findAll();
+  @Put(':id')
+  update(
+      @Param('id') id: string,
+      @Body() updatePetDto: UpdatePetDto,
+  ) {
+      return this.updateUseCase.execute({
+          id,
+          ...updatePetDto,
+      });
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+      return this.deleteUseCase.execute({ id });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.petsService.findOne(+id);
+      return this.getUseCase.execute({ id });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePetDto: UpdatePetDto) {
-    return this.petsService.update(+id, updatePetDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.petsService.remove(+id);
+  @Get()
+  search(@Query() searchParams: SearchPetDto) {
+      return this.listUseCase.execute(searchParams);
   }
 }

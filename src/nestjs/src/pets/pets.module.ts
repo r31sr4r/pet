7814c37bbc4ebca433146b7603cdabd3afcdf43f
r31sr4r <rafael.reis.a@gmail.com>
2022-08-06 21:51:49 +0000

    refactor: add providers

--- src/nestjs/src/pets/pets.module.ts
+++ src/nestjs/src/pets/pets.module.ts
@@ -1,59 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PetsService } from './pets.service';
 import { PetsController } from './pets.controller';
-import {
-    CreatePetUseCase,
-    DeletePetUseCase,
-    GetPetUseCase,
-    ListPetsUseCase,
-    UpdatePetUseCase,
-} from 'pet-core/pet/application';
-import { PetInMemoryRepository } from 'pet-core/pet/infra';
-import PetRepository from 'pet-core/dist/pet/domain/repository/pet.repository';
+import { PET_PROVIDERS } from './pets.providers';
 
 @Module({
     controllers: [PetsController],
     providers: [
         PetsService,
-        {
-            provide: 'PetInMemoryRepository',
-            useClass: PetInMemoryRepository,
-        },
-        {
-            provide: CreatePetUseCase.UseCase,
-            useFactory: (petRepo: PetRepository.Repository) => {
-                return new CreatePetUseCase.UseCase(petRepo);
-            },
-            inject: ['PetInMemoryRepository'],
-        },
-        {
-            provide: UpdatePetUseCase.UseCase,
-            useFactory: (petRepo: PetRepository.Repository) => {
-                return new UpdatePetUseCase.UseCase(petRepo);
-            },
-            inject: ['PetInMemoryRepository'],
-        },
-        {
-            provide: GetPetUseCase.UseCase,
-            useFactory: (petRepo: PetRepository.Repository) => {
-                return new GetPetUseCase.UseCase(petRepo);
-            },
-            inject: ['PetInMemoryRepository'],
-        },
-        {
-            provide: ListPetsUseCase.UseCase,
-            useFactory: (petRepo: PetRepository.Repository) => {
-                return new ListPetsUseCase.UseCase(petRepo);
-            },
-            inject: ['PetInMemoryRepository'],
-        },
-        {
-            provide: DeletePetUseCase.UseCase,
-            useFactory: (petRepo: PetRepository.Repository) => {
-                return new DeletePetUseCase.UseCase(petRepo);
-            },
-            inject: ['PetInMemoryRepository'],
-        },        
+        ...Object.values(PET_PROVIDERS.USE_CASES),
+        ...Object.values(PET_PROVIDERS.USE_CASES)       
         
     ],
 })
---
+++ src/nestjs/src/pets/pets.providers.ts
@@ -0,0 +1,54 @@
+import {PetRepository} from "pet-core/pet/domain";
+import { CreatePetUseCase, DeletePetUseCase, GetPetUseCase, ListPetsUseCase, UpdatePetUseCase } from "pet-core/pet/application";
+import { PetInMemoryRepository } from "pet-core/pet/infra";
+
+export namespace PET_PROVIDERS {
+    export namespace REPOSITORIES {
+        export const PET_IN_MEMORY_REPOSITORY = {
+            provide: 'PetInMemoryRepository',
+            useClass: PetInMemoryRepository,
+        };
+    }
+
+    export namespace USE_CASES {
+        export const CREATE_PET = {
+            provide: 'CreatePetUseCase',
+            useFactory: (categoryRepo: PetRepository.Repository) => {
+                return new CreatePetUseCase.UseCase(categoryRepo);
+            },
+            inject: [REPOSITORIES.PET_IN_MEMORY_REPOSITORY.provide],
+        };
+
+        export const UPDATE_PET = {
+            provide: 'UpdatePetUseCase',
+            useFactory: (categoryRepo: PetRepository.Repository) => {
+                return new UpdatePetUseCase.UseCase(categoryRepo);
+            },
+            inject: [REPOSITORIES.PET_IN_MEMORY_REPOSITORY.provide],
+        };
+
+        export const GET_PET = {
+            provide: GetPetUseCase.UseCase,
+            useFactory: (categoryRepo: PetRepository.Repository) => {
+                return new GetPetUseCase.UseCase(categoryRepo);
+            },
+            inject: [REPOSITORIES.PET_IN_MEMORY_REPOSITORY.provide],
+        };
+
+        export const LIST_CATEGORIES = {
+            provide: 'ListPetsUseCase',
+            useFactory: (categoryRepo: PetRepository.Repository) => {
+                return new ListPetsUseCase.UseCase(categoryRepo);
+            },
+            inject: [REPOSITORIES.PET_IN_MEMORY_REPOSITORY.provide],
+        };
+
+        export const DELETE_PET = {
+            provide: DeletePetUseCase.UseCase,
+            useFactory: (categoryRepo: PetRepository.Repository) => {
+                return new DeletePetUseCase.UseCase(categoryRepo);
+            },
+            inject: [REPOSITORIES.PET_IN_MEMORY_REPOSITORY.provide],
+        };
+    }
+}

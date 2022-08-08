1d91745fd94c53011d8c0669eb4e79ed1347d799
r31sr4r <rafael.reis.a@gmail.com>
2022-08-08 22:09:15 +0000

    test: add more mapper tests

--- src/@core/src/pet/infra/db/sequelize/pet-mapper.spec.ts
+++ src/@core/src/pet/infra/db/sequelize/pet-mapper.spec.ts
@@ -1,4 +1,5 @@
-import { LoadEntityError } from '#seedwork/domain';
+import { Pet } from '#pet/domain';
+import { LoadEntityError, UniqueEntityId } from '#seedwork/domain';
 import { Sequelize } from 'sequelize-typescript';
 import PetModelMapper from './pet-mapper';
 import { PetModel } from './pet-model';
@@ -41,5 +42,47 @@ describe('PetMapper Unit Tests', () => {
             });
             expect(err.message).toBe('An entity could not be loaded');            
         }
+	});
+
+
+	it('should throw a generic error', async () => {
+		const error = new Error('An error has occurred');
+		const spyValidate = jest
+			.spyOn(Pet, 'validate')
+			.mockImplementation(() => {
+				throw error;
+			});
+		const model = PetModel.build({
+			id: '02b090cf-5658-4073-b242-9bf64915b3ad',
+		});
+
+		expect(() => PetModelMapper.toEntity(model)).toThrow(error);
+		expect(spyValidate).toHaveBeenCalled();
+		spyValidate.mockRestore();
+	});
+
+	it('should map a pet model to a pet', async () => {
+		const created_at = new Date();
+		const model = PetModel.build({
+			id: '02b090cf-5658-4073-b242-9bf64915b3ad',
+			name: 'Pet 1',
+			type: 'dog',
+			is_active: true,
+			created_at,
+		});
+        
+		const entity = PetModelMapper.toEntity(model);
+
+		expect(entity.toJSON()).toStrictEqual(
+			new Pet(
+				{
+					name: 'Pet 1',
+					type: 'dog',
+					is_active: true,
+					created_at,
+				},
+				new UniqueEntityId('02b090cf-5658-4073-b242-9bf64915b3ad')
+			).toJSON()
+		);
 	});
 });

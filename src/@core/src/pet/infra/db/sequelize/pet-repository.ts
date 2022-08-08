4233ea9b317ba4522c48b3c7970ec9919c433d7f
r31sr4r <rafael.reis.a@gmail.com>
2022-08-08 23:15:08 +0000

    feat: add search method

--- src/@core/src/pet/infra/db/sequelize/pet-repository.ts
+++ src/@core/src/pet/infra/db/sequelize/pet-repository.ts
@@ -1,18 +1,13 @@
 import { Pet, PetRepository } from '#pet/domain';
 import { NotFoundError, UniqueEntityId } from '#seedwork/domain';
+import { Op } from 'sequelize';
 import PetModelMapper from './pet-mapper';
 import { PetModel } from './pet-model';
 
 export class PetSequelizeRepository implements PetRepository.Repository {
 	constructor(private petModel: typeof PetModel) {}
 
-	sortableFields: string[] = ['name', 'created_at'];
-
-	search(
-		props: PetRepository.SearchParams
-	): Promise<PetRepository.SearchResult> {
-		throw new Error('Method not implemented.');
-	}
+	sortableFields: string[] = ['name', 'type', 'breed']
 
 	async insert(entity: Pet): Promise<void> {
 		await this.petModel.create(entity.toJSON());
@@ -40,4 +35,31 @@ export class PetSequelizeRepository implements PetRepository.Repository {
 			rejectOnEmpty: new NotFoundError(`Entity not found using ID ${id}`),
 		});
 	}
+
+	async search(
+		props: PetRepository.SearchParams
+	): Promise<PetRepository.SearchResult> {
+		const offset = (props.page - 1) * props.per_page;
+		const limit = props.per_page;
+		const { rows: models, count } =
+			await this.petModel.findAndCountAll({
+				...(props.filter && {
+					where: { name: { [Op.like]: `%${props.filter}%` } },
+				}),
+				...(props.sort && this.sortableFields.includes(props.sort)
+					? { order: [[props.sort, props.sort_dir]] }
+					: { order: [['name', 'ASC']] }),
+				offset,
+				limit,
+			});
+		return new PetRepository.SearchResult({
+			items: models.map((m) => PetModelMapper.toEntity(m)),
+			current_page: props.page,
+			per_page: props.per_page,
+			total: count,
+			filter: props.filter,
+			sort: props.sort,
+			sort_dir: props.sort_dir,
+		});
+	}	
 }

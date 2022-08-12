4b8c88dc7ca5481f86e7c6f40a93b886f20d0cf6
r31sr4r <rafael.reis.a@gmail.com>
2022-08-12 00:31:20 +0000

    refactor: :wrench: env variables

---
+++ src/@core/src/@seedwork/infra/config/index.ts
@@ -0,0 +1,8 @@
+
+export const config = {
+    db: {
+        vendor: process.env.DB_VENDOR as any,
+        host: process.env.DB_HOST,
+        logging: process.env.DB_LOGGING === 'true',
+    }    
+}

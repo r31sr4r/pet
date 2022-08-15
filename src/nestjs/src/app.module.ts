78e1f77fab60ade595f395a6ac1d120ba0891a49
r31sr4r <rafael.reis.a@gmail.com>
2022-08-15 21:00:33 +0000

    feat: add config module

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [PetsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot(), PetsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}

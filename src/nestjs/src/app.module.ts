d02e84061ac04197b430233f9aa6735a04a753ca
r31sr4r <rafael.reis.a@gmail.com>
2022-08-22 12:51:37 +0000

    feat: add user resource on nest.js

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [ConfigModule.forRoot(), PetsModule, DatabaseModule],
+    imports: [ConfigModule.forRoot(), PetsModule, DatabaseModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
